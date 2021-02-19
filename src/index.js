f6f237db950640bfe2e4341b07fb59d0ad2a4b74
Jeffrey Martinez <martinezjf2@gmail.com>
2021-02-18 21:21:45 -0500

    added notes

--- src/index.js
+++ src/index.js
@@ -2,4 +2,13 @@ import React from "react";
 import ReactDOM from "react-dom";
 import App from "./App"
 
+
+// Manually refreshing will show the changes, also, any changes to components deeper in the file structure will cause an auto-refresh as expected.
+
+// One workaround noted suggests putting the following in the index.js which enables reloading:
+
+// if (module.hot) {
+//     module.hot.accept();
+// }
+
 ReactDOM.render(<App />, document.getElementById("root"))
